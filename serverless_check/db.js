83f9229c6e895a7d6cdada99fa3b6772855cf0e9
Daniel Mermelstein <danmermel@gmail.com>
2018-03-04 09:14:26 +0000

    added check action

---
+++ serverless_check/db.js
@@ -0,0 +1,21 @@
+var AWS = require('aws-sdk')
+AWS.config.loadFromPath('./config.json');
+
+var dynamodb = new AWS.DynamoDB();
+
+var table = "nottariodb";
+
+var docClient = new AWS.DynamoDB.DocumentClient()
+
+var read = function(id, callback) {
+
+  var obj = { TableName : table,
+	   Key: { "id": id}
+      };
+
+  docClient.get(obj, callback);  
+}
+
+module.exports = {
+  read: read
+};
