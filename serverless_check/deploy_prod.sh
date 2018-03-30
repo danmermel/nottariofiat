19bc5500d06b24a6011b26087d53e2bc9a7331d5
Daniel Mermelstein <danmermel@gmail.com>
2018-03-30 09:56:56 +0100

    deployment

---
+++ serverless_check/deploy_prod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp ../config_prod.json ./config.json
+zip -r nottariofiat.zip package.json index.js db.js config.json node_modules/
+aws lambda update-function-code --function-name nottarioFiatCheckProd --zip-file fileb://nottariofiat.zip
+
+
--- serverless_pay/deploy_test.sh
+++ serverless_pay/deploy_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp ../config_test.json ./config.json
 zip -r nottariofiat.zip package.json index.js db.js queue.js config.json node_modules/
 aws lambda update-function-code --function-name nottarioFiatPayment --zip-file fileb://nottariofiat.zip
 
