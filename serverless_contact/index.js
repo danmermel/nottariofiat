9de6fadb6336f05502d30b2bcb068562f46e2bc1
Daniel Mermelstein <danmermel@gmail.com>
2018-04-04 16:29:22 +0100

    update

--- serverless_contact/index.js
+++ serverless_contact/index.js
@@ -4,13 +4,26 @@ var qs = require('qs');
 
 // Load credentials and set the region from the JSON file
 AWS.config.loadFromPath('./config.json');
+var config = require('./config.json');
 
 exports.handler = function (event, context, callback) {
   var body = qs.parse(event.body);
-  console.log(body);
-  if (typeof body.id == 'undefined') {
-    throw (new Error('missing id'));
-  }
-  callback(null, { statusCode: 200, headers: { 'Access-Control-Allow-Origin': '*'},body:JSON.stringify( {} ) }) 
+  console.log('THE DATA', body);
+  console.log('context',JSON.stringify(context));
+  var sns = new AWS.SNS();
+  var message = body.name + ' ' + body.email + ' ' + body.message
+  sns.publish({
+    Message: message,
+    TopicArn:  config.contactTopic
+  }, function(err, data) {
+    if (err) {
+      console.log(err.stack);
+      callback(err, null);;
+    }
+    console.log('push sent');
+    console.log(data);
+    callback(null, { statusCode: 302, headers: { Location: config.thankYouURL }}) 
+
+  });
 };
 
