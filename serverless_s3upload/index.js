02a6b8f6516222f3fdf801eab8f32dfe7b308aa6
Daniel Mermelstein <danmermel@gmail.com>
2018-09-03 15:16:55 +0100

    comments

--- serverless_s3upload/index.js
+++ serverless_s3upload/index.js
@@ -6,19 +6,22 @@ var kuuid = require('kuuid');
 // Load credentials and set the region from the JSON file
 AWS.config.loadFromPath('./config.json');
 
-  var s3 = new AWS.S3({
-    apiVersion: '2006-03-01',
-    signatureVersion: 'v4'
-  });
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  signatureVersion: 'v4'
+});
 
 exports.handler = function (event, context, callback) {
   
   var params = {Bucket: config.ipfsbucket, 
 		Key: kuuid.id() }; //generates unique key
-  s3.getSignedUrl('putObject', params, function (err, url) {
+  //generates a presigned url that allows the writer to PUT the object with 
+  // the above key to the bucket in theconfig
+  s3.getSignedUrl('putObject', params, function (err, url) {  
     console.log('Error is ', err);
     console.log('The URL is', url);
-     var obj = {"url":url};  //create an object because the return expects an object
+    var obj = {"url":url};  
+    //create an object because Lambda + API gateway expects an object being returned
     callback(null, { statusCode: 200, 
                      headers: { 'Access-Control-Allow-Origin': '*'},
                      body:JSON.stringify(obj)
