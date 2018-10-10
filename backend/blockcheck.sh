9736e70198eb295ed1e81b459fb26708dd61fe11
Daniel Mermelstein <danmermel@gmail.com>
2018-10-10 16:13:54 +0100

    block status checker

---
+++ backend/blockcheck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+domain="api"
+if [ $HOSTNAME == 'windermere' ]
+  then
+    domain="rinkeby"
+  fi
+
+hexblock=`curl -s "https://${domain}.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=YourApiKeyToken" | jq -r .result`
+decblock=$(($hexblock))
+
+ourhexblock=`curl -s -X POST -H 'Content-type: application/json' --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}' http://localhost:8545 | jq -r .result`
+ourdecblock=$(($ourhexblock))
+echo "$ourdecblock / $decblock"
+let blockdiff=$decblock-$ourdecblock
+echo "Diff is $blockdiff"
+if [ $blockdiff -ge 100 ]
+  then
+   aws sns publish --topic-arn arn:aws:sns:eu-west-1:160991186365:webmails --message "Out of sync by $blockdiff blocks on $HOSTNAME"
+fi
