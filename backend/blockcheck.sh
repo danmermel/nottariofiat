f143e573031d30279b794cb8012fb167d09e330f
Daniel Mermelstein <danmermel@gmail.com>
2018-10-10 16:38:02 +0100

    adding PATH for cron

--- backend/blockcheck.sh
+++ backend/blockcheck.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/daniel/.local/bin
+
 domain="api"
 if [ $HOSTNAME == 'windermere' ]
   then
